43014770fb051e4b26b78f729803f62eb3dcdfbe
jeffrudderman <soundartsandmusic@gmail.com>
2015-07-23 01:54:58 -0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -26,10 +26,7 @@ os.environ["STRING_INTRO"] = "As a surprise gift for Katie and Jason at their we
 
 global_data = {
     "VIDEOS": [{
-        "question": "The secret to a long, happy marriage. \n\
-        - Advice for a groom, bride or both. \n\
-        - General congratulations and best wishes. \n\
-        - Words of wisdom on life and/or marriage.",
+        "question": "The secret to a long, happy marriage.- Advice for a groom, bride or both.- General congratulations and best wishes.- Words of wisdom on life and/or marriage.".split("."),
         "limit": 90,
         "required": True
     }, {
