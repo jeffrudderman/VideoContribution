c05a2c4832db41f8dddcb7d15ae8059403484798
jeffrudderman <soundartsandmusic@gmail.com>
2015-09-21 09:44:19 -0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -26,7 +26,7 @@ os.environ["STRING_INTRO"] = "Let's get started with the basics."
 
 global_data = {
     "VIDEOS": [{
-        "question": "What is the secret to a long, happy marriage? \n Words of “Wizdom” on marriage and/or life. \n Offer your congratulations and best wishes.",
+        "question": "What is the secret to a long, happy marriage? \n Words of Wizdom on marriage and/or life. \n Offer your congratulations and best wishes.",
         "limit": 90,
         "required": True
     }],
